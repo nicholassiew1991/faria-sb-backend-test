70c4562d3320abcc3d1acf4888148b10ebd6510e
nicholassiew1991 <nicholas_siew1991@hotmail.com>
2022-05-10 16:25:10 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+using SB.CoreTest;
+
+/// <summary>
+/// SchoolsBuddy Technical Test.
+///
+/// Your task is to find the highest floor of the building from which it is safe
+/// to drop a marble without the marble breaking, and to do so using the fewest
+/// number of marbles. You can break marbles in the process of finding the answer.
+///
+/// The method Building.DropMarble should be used to carry out a marble drop. It
+/// returns a boolean indicating whether the marble dropped without breaking.
+/// Use Building.NumberFloors for the total number of floors in the building.
+///
+/// A very basic solution has already been implemented but it is up to you to
+/// find your own, more efficient solution.
+///
+/// Please use the function Attempt2 for your answer.
+/// </summary>
+namespace SB.TechnicalTest
+{
+    class Program
+    {
+        /// <summary>
+        /// Main entry point.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Attempt 1 Highest Safe Floor: {Attempt1()}");
+            Console.WriteLine($"Attempt 1 Total Drops: {Building.TotalDrops}");
+
+            Console.WriteLine();
+            Building.Reset();
+
+            Console.WriteLine($"Attempt 2 Highest Safe Floor: {Attempt2()}");
+            Console.WriteLine($"Attempt 2 Total Drops: {Building.TotalDrops}");
+        }
+
+        /// <summary>
+        /// First attempt - start at first floor and work up one floor at a time
+        /// until you reach a floor at which marble breaks.
+        /// The highest safe floor is one below this.
+        /// </summary>
+        /// <returns>Highest safe floor.</returns>
+        static int Attempt1()
+        {
+            var i = 0;
+            while (++i <= Building.NumberFloors && Building.DropMarble(i));
+
+            return i - 1;
+        }
+
+        /// <summary>
+        /// Second attempt - you do this one!
+        /// </summary>
+        /// <returns>Highest safe floor.</returns>
+        static int Attempt2()
+        {
+            return 0;
+        }
+    }
+}
